928e451975e22852f5d27d990829cd3d67454e9d
sanggu.yeo <sangkuyeo723@naver.com>
2022-02-14 14:58:16 +0900

    [Sanggu] Add basic code

---
+++ aceCoder/aceCoder/member.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include"pch.h"
+#include<string>
+using namespace std;
+
+enum CL {
+	CL1 = 1,
+	CL2,
+	CL3,
+	CL4
+};
+
+enum CERTI {
+	ADV = 0,
+	PRO,
+	EX
+};
+
+class member {
+	unsigned int employeeNum;
+	string name;
+	CL cl;
+	string phoneNum;
+	unsigned int birthday;
+	CERTI certi;
+};
+
+int add();
+int del();
+int search();
+int modify();
